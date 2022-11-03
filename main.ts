a41184366baaf141b84467376ff2e1f2160c00c4
mhadhebbenameur <117409966+mhadhebbenameur@users.noreply.github.com>
2022-11-03 23:35:36 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+let soundLevel = 0
+let maxSound = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        maxSound += input.soundLevel()
+        basic.showNumber(maxSound)
+    } else {
+        soundLevel = input.soundLevel()
+        led.plotBarGraph(
+        soundLevel,
+        255
+        )
+        if (soundLevel > maxSound) {
+            maxSound = soundLevel
+        }
+    }
+})
